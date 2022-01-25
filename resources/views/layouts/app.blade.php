4818893dcbd3c05f2e7c8ef3b0f5ec609fb3dae9
Lucas Sheridan <lucas_sheridan@me.com>
2022-01-25 15:32:14 -0500

    Fixed Html Title and terrible cat ui adjustments 2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>Artifacts ONline</title>
+        <title>BSGE | Artifacts</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
